baae3452c78939e708f5edd80b614a5661830654
Frank Smit <frank@61924.nl>
2015-10-25 22:35:41 +0100

    Rename ds executable.

---
+++ src/a/ds/cmd/ds_cli/ds_cli.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"image/png"
+	"os"
+
+	"a/ds"
+)
+
+func main() {
+	m, err := ds.NewMap(12.0, 5.5, 24.82, 42.2, 122.9)
+	if err != nil {
+		panic(err)
+	}
+
+	m.Generate(0.3)
+
+	fd, err := os.Create("test.png")
+	if err != nil {
+		panic(err)
+	}
+
+	w := bufio.NewWriter(fd)
+
+	err = png.Encode(w, m.Image())
+	if err != nil {
+		panic(err)
+	}
+
+	w.Flush()
+	fd.Close()
+}
